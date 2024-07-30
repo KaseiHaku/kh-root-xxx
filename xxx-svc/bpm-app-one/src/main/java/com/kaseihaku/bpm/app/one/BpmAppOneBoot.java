e4dc4a102cca72df25eed32962713d59f36f0f39
kasei <kasei@kaseihaku.com>
2024-07-30 16:05:48 +0800

    add redis-standalone config

--- xxx-svc/bpm-app-one/src/main/java/com/kaseihaku/bpm/app/one/BpmAppOneBoot.java
+++ xxx-svc/bpm-app-one/src/main/java/com/kaseihaku/bpm/app/one/BpmAppOneBoot.java
@@ -1,12 +1,19 @@
 package com.kaseihaku.bpm.app.one;
 
 
+import com.kaseihaku.boot.starter.redis.autoconfigure.RedisPropertySourceAutoConfiguration;
 import org.mybatis.spring.boot.autoconfigure.MybatisAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.mybatis.spring.annotation.MapperScan;
 
-@SpringBootApplication(exclude = {MybatisAutoConfiguration.class})
+@SpringBootApplication(exclude = {
+    /* 框架默认 redis 是 cluster 模式，如果想要 standalone 模式需要把该行放出来
+     * 并且把 application.yml 中 spring.data.redis 部分注释放出来
+     * */
+    RedisPropertySourceAutoConfiguration.class,
+    MybatisAutoConfiguration.class
+})
 @MapperScan(basePackages = {"com.kaseihaku.bpm.app.one.repo.dao.mapper"})
 public class BpmAppOneBoot {
 
